7b10c1dd98cb6e550755fb28af52f1a7c79edf68
Sergey <s.shakirov@avtomatika.kz>
2025-02-06 23:31:44 +0500

    Наведение порядка

--- app/src/main/java/com/example/justdo/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/example/justdo/presentation/screens/LoginScreen.kt
@@ -2,7 +2,6 @@ package com.example.justdo.presentation.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
